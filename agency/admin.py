89232cb99eaadd9f66f69698a94233753fa0d672
Oleksandr Mazur <a9p9m9@gmail.com>
2025-05-04 11:25:49 +0300

    enable and customize Django admin panel
    - Uncomment path to admin in urls.py
    - Add "django.contrib.admin" to INSTALLED_APPS
    - Create superuser for admin access
    - Register models: Redactor, Newspaper and Topic in admin.py

--- agency/admin.py
+++ agency/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from agency.models import Topic, Redactor, Newspaper
+
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    search_fields = ("username", "first_name", "last_name",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("years_of_experience",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "years_of_experience",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    search_fields = ("title",)
+    list_display = ("title", "published_date")
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ("name",)
