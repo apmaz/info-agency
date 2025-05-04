9e811e5a797817efa2f1e21da1ec8a1572e55664
Oleksandr Mazur <a9p9m9@gmail.com>
2025-05-04 10:57:24 +0300

    add models Topic, Redactor and Newspaper
    - Add Topic model with __str__ method
    - Add Redactor model with Meta class, get_absolute_url and __str__
    - Add Newspaper model with __str__, ManyToMany to Topic and Redactor
    
    - Set AUTH_USER_MODEL = "agency.Redactor"
    - Register "agency" in INSTALLED_APPS
    
    - Install black for code formatting
    - Format code with black: manage.py, agency/, and information_agency/
    
    - Add db.sqlite3 to .gitignore

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+from info_agency import settings
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = "redactor"
+        verbose_name_plural = "redactors"
+
+    def get_absolute_url(self):
+        return reverse("agency:redactor-detail", kwargs={"pk": self.pk})
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateField(auto_now_add=True)
+    topic = models.ManyToManyField(Topic, related_name="newspapers_by_topic")
+    publishers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="newspapers_by_publisher"
+    )
+
+    def __str__(self):
+        return self.title
